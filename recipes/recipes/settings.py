e7e2dfb5ea8c56f8a9e55d34e8c77dcbe7055a07
Andrei Beliaev <ab5030107021@yandex.ru>
2023-10-28 13:52:52 +0300

    master try to deploy

--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p0%e7fo1x8yvqtys6e@p_ywh9@nocrx&r7y%y&204agm6ss337'
+# SECRET_KEY = 'django-insecure-p0%e7fo1x8yvqtys6e@p_ywh9@nocrx&r7y%y&204agm6ss337'
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'andrew107021.pythonanywhere.com',
+]
+
+INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
@@ -76,10 +83,24 @@ WSGI_APPLICATION = 'recipes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'andrew107021$default',
+        'USER': 'andrew107021',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'andrew107021.mysql.pythonanywhere-services.com',
+        # 'OPTIONS': {
+        #     'init-command': "SET NAMES 'utf8mb4; SET sql_mode='STRICT_TRANS_TABLES'",
+        #     'charset': 'utf8mb4',
+        # }
     }
 }
 
