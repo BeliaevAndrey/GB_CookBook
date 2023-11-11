80bcfa81810c59b45070644a9525c2ab4dc8d05c
Andrei Beliaev <ab5030107021@yandex.ru>
2023-11-11 23:57:20 +0300

    html refactored

--- recipes/cookbook/forms.py
+++ recipes/cookbook/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class AddRecipeForm(forms.Form):
     """ Add a recipe to data base """
-    name = forms.CharField(max_length=100, widget=forms.Textarea(attrs={'class': 'recipe__name'}))
+    name = forms.CharField(max_length=80, widget=forms.Textarea(attrs={'class': 'recipe__name'}))
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'recipe__field'}), max_length=500)
     ingredients = forms.CharField(widget=forms.Textarea(attrs={'class': 'recipe__field'}))
     steps = forms.CharField(widget=forms.Textarea(attrs={'class': 'recipe__field'}))
