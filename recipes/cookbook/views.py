6b058d23edea3b1af3ff0a6463dc6228743bfd0c
BeliaevAndrey <ab5030107021@yandex.ru>
2025-07-30 12:03:12 +0300

    cookbook: categories view refactored

--- recipes/cookbook/views.py
+++ recipes/cookbook/views.py
@@ -38,12 +38,21 @@ class AllRecipes(ListView):
     extra_context = {"title": "Все рецепты"}
 
 
-class Categories(ListView):
-    model = Category
+class Categories(TemplateView):
     template_name = 'cookbook/categories.html'
-    context_object_name = 'category'
-    ordering = "pk"
-    extra_context = {"title": "Категории"}
+
+    def get_context_data(self, **kwargs):
+        categories = [ctg for ctg in Category.objects.all()]
+        recipes_by_category = {
+            ctg.title: [recipe for recipe in Recipe.objects.all() if recipe.category == ctg]
+            for ctg in categories
+        }
+        context = {
+            "title": "Категории",
+            "categories": recipes_by_category
+        }
+
+        return context
 
 
 class AddRecipe(LoginRequiredMixin, TemplateView):
@@ -94,56 +103,3 @@ class RecipePage(DetailView):
             "ingredients": ingredients,
         }
         return render(request, self.template_name, context)
-
-
-# def index(request):
-#     recipes = list(Recipe.objects.all())
-#     if not recipes:
-#         return render(request,
-#                       'cookbook/index_empty.html')
-#     if len(recipes) > 5:
-#         choices(recipes, k=5)
-#     context = {
-#         'title': 'Случайные рецепты',
-#         'recipes': recipes,
-#     }
-#     return render(request,
-#                   'cookbook/index.html',
-#                   context=context)
-#
-#
-#
-# @login_required(login_url='login')
-# def add_recipe_form(request):
-#     title = "Add recipe"
-#     message = ""
-#     if request.method == 'POST':
-#         form = AddRecipeForm(request.POST)
-#         if form.is_valid():
-#             name = form.cleaned_data['name']
-#             description = form.cleaned_data['description']
-#             steps = form.cleaned_data['steps']
-#             duration = form.cleaned_data['duration']
-#             ingredients = form.cleaned_data['ingredients']
-#             author = get_user_model().objects.get(pk=request.user.pk)
-#             Recipe(
-#                 name=name,
-#                 description=description,
-#                 steps=steps,
-#                 duration=duration,
-#                 ingredients=ingredients,
-#                 author=author,
-#             ).save()
-#     else:
-#         message = "Add a recipe"
-#         form = AddRecipeForm()
-#     return render(request, 'cookbook/add_recipe.html',
-#                   {'form': form, 'title': title, 'message': message})
-#
-#
-# def all_recipes(request):
-#     pass
-#
-#
-# def one_recipe(request):
-#     pass
