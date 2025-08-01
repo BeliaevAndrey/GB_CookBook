2d41922738efa49655840c1e18b7097ad064817d
BeliaevAndrey <ab5030107021@yandex.ru>
2025-08-01 23:42:33 +0300

    update: cookbook.admin updated

--- recipes/cookbook/admin.py
+++ recipes/cookbook/admin.py
@@ -1,3 +1,63 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Recipe)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ordering = ('title', 'description')
+    list_filter = ('title',)
+    search_fields = ('title',)
+    readonly_fields = []
+
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['title', 'description']
+            }
+        ),
+        (
+            'Details',
+            {
+                'classes': ['collapse'],
+                'fields': ['description']
+            }
+        )
+    ]
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'author')
+    ordering = ('name', 'category', 'author', 'add_date')
+    list_filter = ('name', 'category')
+    readonly_fields = ['author', 'add_date']
+
+    fieldsets = [
+        (
+            None,
+         {
+             'classes': ['wide'],
+             'fields': ['name', 'category', 'description', 'author']
+         }
+         ),
+        (
+            'Details',
+            {
+                'classes': ['collapse'],
+                'fields': [
+                    'name',
+                    'category',
+                    'description',
+                    'ingredients',
+                    'steps',
+                    'duration',
+                    'image',
+                    'author',
+                    'add_date'
+                ]
+            }
+        ),
+    ]
